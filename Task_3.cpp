35f2238a33fa3287a15f1fecbde1a3db323b078f
Ebadulislam1001 <ebadulislamfarooqi@gmail.com>
2023-07-24 23:13:06 +0530

    Task 3 added

---
+++ Task_3.cpp
@@ -0,0 +1,25 @@
+// 3. Make a Number Guessing  game in C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    int number = (rand() % 128) + 1;
+    int guess = 0;
+    do
+    {
+        cout << "\nEnter your Guess : ";
+        cin >> guess;
+        if (guess < number)
+            cout << "Your guess is too low\n";
+        else if (guess > number)
+            cout << "Your guess is too high\n";
+        else
+            cout << "\nYour guess is right";
+    } while (guess != number);
+
+    cout << "\n\n";
+    return 0;
+}
